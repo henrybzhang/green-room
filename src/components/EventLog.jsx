d0b46a8636df1daba61cda66752301901b50dba1
hydesk <henrybzhang.99@gmail.com>
2022-06-12 10:48:43 -0500

    Readd scrolling for event log

--- src/components/EventLog.jsx
+++ src/components/EventLog.jsx
@@ -2,8 +2,6 @@ import { useContext, useState, useEffect } from 'react';
 import '../styles/EventLog.css';
 import { ActionContext } from '../providers/ActionProvider';
 
-const MAX_EVENTS = 20;
-
 const initialLog = [
   'Welcome to a recovering world',
   'The surrounding area is piled high with trash and is extremely polluted. Smog covers the land, making the visibility almost nil.',
@@ -18,7 +16,7 @@ function EventLog() {
       return;
     }
 
-    let eventLogToKeep = eventLog.slice(0, MAX_EVENTS);
+    let eventLogToKeep = eventLog;
     if (environmentLevel === 7) {
       eventLogToKeep = [];
     }
@@ -31,9 +29,6 @@ function EventLog() {
     <div className="log">
       {eventLog.map((eventText, index) => (
         <div
-          style={{
-            opacity: (MAX_EVENTS - index) / MAX_EVENTS,
-          }}
           // eslint-disable-next-line react/no-array-index-key
           key={index}
         >
